66c3883c254e4045fd9510826b8f5bfd09451d70
AdamTheThirst <chatbotsadmn@gmail.com>
2024-09-29 21:25:36 +0300

    create base view

--- cities/views.py
+++ cities/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+from .models import City
 # Create your views here.
+
+__all__ = (
+    'index',
+)
+
+def index(request):
+    ds = City.objects.all()
+
+    context = {
+        'ds': ds
+    }
+
+    return render(request, 'index.html', context)
