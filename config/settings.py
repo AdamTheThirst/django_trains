3c06b2252ff0808cfbc7bc20f1c8dc4783e69265
AdamTheThirst <chatbotsadmn@gmail.com>
2024-09-29 19:37:08 +0300

    add cities app to settings.py

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+PROJECT_APP = [
+    'cities.apps.CitiesConfig',
+]
+
+INSTALLED_APPS = DJANGO_APPS + PROJECT_APP
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
