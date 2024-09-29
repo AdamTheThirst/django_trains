3922d7f8fc8f2c012ffd00c7aae6f5549f06f7e3
AdamTheThirst <chatbotsadmn@gmail.com>
2024-09-29 19:34:32 +0300

    creae cities model in models.py

--- cities/models.py
+++ cities/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class City(models.Model):
+    city = models.CharField(max_length=100, unique=True, verbose_name='Город')
+
+    def __str__(self):
+        return self.city
+
+    class Meta:
+        verbos_name = 'Город'
+        verbos_name_plural = 'Города'
+        ordering = ['city']
