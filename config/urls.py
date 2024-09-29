26c018d5887e14ee2e11f38a18d5d5024953a84c
AdamTheThirst <chatbotsadmn@gmail.com>
2024-09-29 22:45:05 +0300

    nothing special

--- config/urls.py
+++ config/urls.py
@@ -17,8 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-
+from .views import home
 urlpatterns = [
-    path('', include('cities.urls', namespace='cities_main')),
+    path('', home, name='home'),
+    path('cities/', include('cities.urls', namespace='cities_main')),
     path('admin/', admin.site.urls),
 ]
