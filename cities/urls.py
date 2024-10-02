bed3ecd2a6ebca6bbd1567cfa6acc322c06393d4
AdamTheThirst <chatbotsadmn@gmail.com>
2024-10-02 08:06:17 +0300

    add DetailClassView for cities

--- cities/urls.py
+++ cities/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
 from .views import *
+from .views import CityDetailView
 
 app_name = 'cities'
 
 urlpatterns = [
     path('', cities_view, name='cities_index'),
-    path('<int:pk>/', cities_view, name='cities_detail'),
+    path('<int:pk>/', CityDetailView.as_view(), name='cities_detail'),
+
 ]
--- cities/views.py
+++ cities/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
 
 from .models import City
 # Create your views here.
@@ -9,19 +10,23 @@ __all__ = (
 
 def cities_view(request, pk: int = None):
 
-    if pk:
-        ds = City.objects.filter(id=pk).first()
-
-        if ds is None:
-            ds = City.objects.first()
-
-        context = {
-            'ds': ds,
-        }
-        return render(request, 'detales.html', context)
+    # if pk:
+    #     ds = City.objects.filter(id=pk).first()
+    #
+    #     if ds is None:
+    #         ds = City.objects.first()
+    #
+    #     context = {
+    #         'ds': ds,
+    #     }
+    #     return render(request, 'detales.html', context)
 
     ds = City.objects.all()
     context = {
         'ds': ds
     }
     return render(request, 'cities.html', context)
+
+class CityDetailView(DetailView):
+    qeryset = City.objects.all()
+    template_name = 'detales.html'
