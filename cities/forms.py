e341b884459dc8ea29f45d3698ada5ce8fa9bb07
AdamTheThirst <chatbotsadmn@gmail.com>
2024-10-02 15:28:42 +0300

    update forms

--- cities/forms.py
+++ cities/forms.py
@@ -4,7 +4,11 @@ from cities.models import City
 
 
 class CityForm(forms.ModelForm):
-    city = forms.CharField(label='Добавить город')
+    city = forms.CharField(label='Добавить город',
+                           widget=forms.TextInput(attrs={
+                               'class': 'form-control',
+                               'placeholder': 'Введите название города',
+                           }))
 
     class Meta:
         model = City
