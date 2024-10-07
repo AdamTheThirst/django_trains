ba6e4ec08b2758b3b570b4cceb172446403030d0
AdamTheThirst <chatbotsadmn@gmail.com>
2024-10-07 23:10:11 +0300

    update view by def get_routes

--- routes/views.py
+++ routes/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib import messages
 
 from routes.forms import RouteForm
+from routes.utils import get_routes
 
 
 # Create your views here.
@@ -15,7 +16,13 @@ def find_routes(request):
     if request.method == 'POST':
         form = RouteForm(request.POST)
         if form.is_valid():
-            ...
+            try:
+                context = get_routes(request, form)
+            except ValueError as e:
+                messages.error(request, e)
+                return render(request, 'routes/display_list.html', {form: form})
+            return render(request, 'routes/display_list.html', context)
+
     else:
         form = RouteForm()
         messages.error(request, 'Нет данных для поиска')
