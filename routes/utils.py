29fd6996285696c10edd374b852b85a3acad80b4
AdamTheThirst <chatbotsadmn@gmail.com>
2024-10-09 23:45:07 +0300

    update

--- routes/utils.py
+++ routes/utils.py
@@ -1,5 +1,15 @@
 from buses.models import Bus
 
+def dfs_path(graph: dict, start, goal):
+    stack = [(start, [start])]
+    while stack:
+        (vertex, path) = stack.pop()
+        if vertex in graph.keys():
+            for next_ in graph[vertex] - set(path):
+                if next_ == goal:
+                    yield path + [next_]
+                else:
+                    stack.append((next_, path + [next_]))
 
 def get_graph(qs) -> dict:
     graph = {}
@@ -9,5 +19,14 @@ def get_graph(qs) -> dict:
     return graph
 
 def get_routes(request, form) -> dict:
+    context = {'form': form}
     qs = Bus.objects.all()
     graph = get_graph(qs)
+    data = form.cleaned_data
+    from_city = data['from_city']
+    to_city = data['to_city']
+    route_travel_time = data['route_travel_time']
+    all_ways = dfs_path(graph, from_city.id, to_city.id)
+    if not len(list(all_ways)):
+        raise ValueError('Маршрута нет')
+    return context
