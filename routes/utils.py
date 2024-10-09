0923d3d257af12f614dc629ed97e0330ac2ee0d1
AdamTheThirst <chatbotsadmn@gmail.com>
2024-10-09 23:29:36 +0300

    add function to create graph

--- routes/utils.py
+++ routes/utils.py
@@ -1,2 +1,13 @@
+from buses.models import Bus
+
+
+def get_graph(qs) -> dict:
+    graph = {}
+    for q in qs:
+        graph.setdefault(q.from_city_id, set())
+        graph[q.from_city_id].add(q.to_city_id)
+    return graph
+
 def get_routes(request, form) -> dict:
-    ...
+    qs = Bus.objects.all()
+    graph = get_graph(qs)
