b1e77c180177137eaad856e7a36b5bfa2c46ab11
AdamTheThirst <chatbotsadmn@gmail.com>
2024-10-26 22:46:33 +0300

    update

--- routes/forms.py
+++ routes/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 
+from buses.models import Bus
 from cities.models import City
 
 
@@ -26,6 +27,33 @@ class RouteForm(forms.Form):
                                             widget=forms.SelectMultiple(
                                                 attrs={
                                                     'class': 'form-control js-example-basic-multiple',
+                                                }
+                                            ),
+                                            required=False)
+
+
+class RouteModelForm(forms.ModelForm):
+
+    name = forms.CharField(label='Название маршрута',
+                           widget=forms.TextInput(attrs={
+                               'class': 'form-control',
+                               'placeholder': 'Название маршрута',
+                           }))
+
+    from_city = forms.ModelChoiceField(label='Откуда ехать', queryset=City.objects.all(),
+                           widget=forms.HiddenInput())
+
+    to_city = forms.ModelChoiceField(label='Куда ехать', queryset=City.objects.all(),
+                           widget=forms.HiddenInput())
+
+    route_travel_time = forms.IntegerField(label='Время в пути',
+                           widget=forms.HiddenInput())
+
+    buses = forms.ModelMultipleChoiceField(queryset=Bus.objects.all(),
+                                            label='Через города',
+                                            widget=forms.SelectMultiple(
+                                                attrs={
+                                                    'class': 'form-control d-done',
                                                 }
                                             ),
                                             required=False)
