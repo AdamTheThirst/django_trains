e904f97518b05116a555c95975f2ad06c16e3e4a
AdamTheThirst <chatbotsadmn@gmail.com>
2024-11-04 20:01:30 +0300

    update

--- routes/views.py
+++ routes/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.views.generic import ListView
 
 from buses.models import Bus
 from cities.models import City
 from routes.forms import RouteForm, RouteModelForm
+from routes.models import Route
 from routes.utils import get_routes
 
 
@@ -73,3 +75,8 @@ def save_route(request):
     else:
         messages.error(request, 'Some problems')
         return redirect('/')
+
+class RouteListView(ListView):
+    paginate_by = 5
+    model = Route
+    template_name = 'routes/list.html'
