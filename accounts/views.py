b76538065fc37104836c2a627a58c00fed717000
AdamTheThirst <chatbotsadmn@gmail.com>
2024-11-08 23:28:12 +0300

    update

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,16 @@
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render
 
+from accounts.forms import UserLoginForm
+
+
 # Create your views here.
+
+def login_view(request):
+    form = UserLoginForm(request.POST or None)
+    if form.is_valid():
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(username=username, password=password)
+        login(request, user)
+    return render(request, 'accounts/login.html', {'form': form})
