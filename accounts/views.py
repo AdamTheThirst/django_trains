98ce2d9a6556c742114c61d9843bcfde9e50aa99
AdamTheThirst <chatbotsadmn@gmail.com>
2024-11-08 23:31:31 +0300

    update

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import authenticate, login
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from accounts.forms import UserLoginForm
 
@@ -8,9 +8,12 @@ from accounts.forms import UserLoginForm
 
 def login_view(request):
     form = UserLoginForm(request.POST or None)
+    _next = request.GET.get('next')
     if form.is_valid():
         username = form.cleaned_data.get('username')
         password = form.cleaned_data.get('password')
         user = authenticate(username=username, password=password)
         login(request, user)
+        _next = _next or '/'
+        return redirect(_next)
     return render(request, 'accounts/login.html', {'form': form})
