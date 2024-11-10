2651a4bc51ea871f4753ffafba8a5a2737ae1ea0
AdamTheThirst <chatbotsadmn@gmail.com>
2024-11-10 23:31:33 +0300

    update

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,9 @@ from django.contrib.auth import logout
 from django.urls import path
 
 from accounts.views import login_view, logout_view
+from cities.urls import app_name
+
+app_name = 'accounts'
 
 urlpatterns = [
     path('login/', login_view, name='login'),
